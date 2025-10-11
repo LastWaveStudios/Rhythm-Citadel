89f0403d2fa0ca59ad0e64ff0b589b54acbaab15
picugeo27 <61882288+picugeo27@users.noreply.github.com>
2025-10-11 13:58:15 +0200

    v0.2.0 - Creación del path
    Está el script que crea paths, junto con los puntos de ancla. Para que funcione hay que poner puntos de ancla decirle en que dirección va el path y cuantas casillas. Hay un script para probar que va, se va a borrar asi que ignorarlo. En el playground se puede ver la jerarquia para crear un path funcional

---
+++ Assets/Src/Gameplay/World/TestingScript.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// <summary>
+/// 
+/// Script que use para ir probando que funcionan las funciones porque no aprendi a usar los test unitarios
+/// 
+/// </summary>
+public class TestingScript : MonoBehaviour
+{
+
+    public Tilemap tilemap;
+    public GameObject miPath;
+    public List<TileBase> paths;
+    private Path script;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        script = new Path();
+        paths = new List<TileBase>();
+        paths = script.GeneratePath(miPath, tilemap);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
