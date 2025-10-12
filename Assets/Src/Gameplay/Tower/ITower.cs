568b47158e3c777e13c9034e4fb00107536d4ad2
Ángela Fernández <angela.fyh.16@gmail.com>
2025-10-12 19:04:07 +0200

    V0.4.0

---
+++ Assets/Src/Gameplay/Tower/ITower.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public abstract class ITower
+{
+   //REHACER INTERFAZ CON LA NUEVA TARJETA
+    /*private float speedAtack;
+    private int typeOfDamage;
+    private float cost;
+    private int area;   //No estoy segura del tipo de la variable
+
+    public void attack() { }*/
+}
