55f7a467102d063844add2ec05f1985f4160a7a0
ExcellionRequiem <130091012+ExcellionRequiem@users.noreply.github.com>
2025-10-12 02:23:38 +0200

    Worldmanager 0.1.0 + Unity 59f2

--- Assets/Src/WorldManager.cs
+++ Assets/Src/WorldManager.cs
@@ -1,72 +1,56 @@
 using System;
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
-public enum Direction
-{
-    Up,
-    Down,
-    Left,
-    Right
-}
+using UnityEngine.Tilemaps;
 
 public class WorldManager : Utilities.Singleton<WorldManager>
 {
-    public Direction[][] paths; 
+
+    [SerializeField] private List<GameObject> pathObjects;
+    [SerializeField] private Tilemap tilemap;
+    private List<Path> paths;
+    private List<Path> pathsWithEnemies;
 
     //Initialize the Direction array array
     void Start()
     {
-        paths = new Direction[0][];
-    }
-
-    //Should be called by the level config/level manager whenever a new level is started to clear out the array
-    public void NewLevel(int levelPathCount){
-        paths = new Direction[levelPathCount][];
-
-        for (int i = 0; i < levelPathCount; i++)
-        {
-            paths[i] = new Direction[0];
-        }
-    }
-
-    //Level manager should use this function to add paths, alternatively add path generation here
-    public void AddPath(int pathID, Direction[] path) {
-        paths[pathID] = path;
-    }
-
-    //Returns a whole path as direction array when given a path ID
-    public Direction[] GetPath(int pathID)
-    {
-        return paths[pathID];
-    }
-
-    //Returns the NEXT direction of the path to an enemy that calls using its assigned path's ID and their current step
-    public Direction GetDirectionFromPath(int pathID, int currentIndex)
-    {
-        return paths[pathID][currentIndex + 1]; //As I understand, it should return the NEXT direction to the enemy?
+        paths = new List<Path>();
+        InitPaths();
     }
 
     void Update()
     {
 
-        //Test, remove when adding paths properly
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            NewLevel(2);
-            AddPath(0, new Direction[] { Direction.Up, Direction.Right, Direction.Left, Direction.Down });
-            AddPath(1, new Direction[] {Direction.Up, Direction.Up, Direction.Down, Direction.Down, Direction.Left,
-                                        Direction.Right, Direction.Left, Direction.Right});
+    }
 
-            Debug.Log("Path 0:" + string.Join(", ", GetPath(0)));
-            Debug.Log("Path 1:" + string.Join(", ", GetPath(1)));
-            
-            Debug.Log("Path 0 step 1:" + " " + GetDirectionFromPath(0, 0));
-            Debug.Log("Path 1 step 5:" + " " + GetDirectionFromPath(1, 4));
+    void AnchorPoints()
+    {
+
+    }
+    TileBase GetDirectionFromPath(int pathID, int currentIndex)
+    {
+        return paths[pathID].GetTileList()[currentIndex];
+    }
+    void InitPaths()
+    {
+        int x = 0;
+        foreach (GameObject pathObject in pathObjects)
+        {
+            paths.Add(new Path());
+            paths[x].GeneratePath(pathObject, tilemap);
+            x++;
         }
-        
-        
+
+        //test
+
+        Debug.Log("Path Getdirection test (1):"+GetDirectionFromPath(0, 0));
+        Debug.Log("Path Getdirection test (1+1):"+GetDirectionFromPath(0, 1));
+    }
+    void UpdateEnemiesOnPath(Path path)
+    {
 
     }
 }
