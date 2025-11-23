230698941bb6fdec4f294c10ba662127120cbd30
picugeo27 <61882288+picugeo27@users.noreply.github.com>
2025-11-23 15:54:57 +0100

    miniFix
    deleted a linea used in an older version

--- Assets/Src/UI/Menus/BallerinaController.cs
+++ Assets/Src/UI/Menus/BallerinaController.cs
@@ -4,7 +4,6 @@ using UnityEngine.EventSystems;
 public class BallerinaController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Animator _animator;
-    private float lastPoint = 0f;
     public void OnPointerEnter(PointerEventData eventData)
     {
         _animator.speed = 1f;
