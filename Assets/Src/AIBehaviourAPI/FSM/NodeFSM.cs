bcbd53225f755a358ee60e6d5ee9ac557d6b36d6
hotdogwithdog <hotdogwithdog@gmail.com>
2025-11-26 20:48:41 +0100

    Added the Behaviour tree engine to the API and 2 examples of it

--- Assets/Src/AIBehaviourAPI/FSM/NodeFSM.cs
+++ Assets/Src/AIBehaviourAPI/FSM/NodeFSM.cs
@@ -2,39 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AIBehaviourAPI.FSM
+namespace AIBehaviourAPI.Fsm
 {
     public class NodeFSM : INode
     {
         protected Action _action;
-        protected List<ITransition> _inputTransitions;
-        protected List<ITransition> _outputTransitions;
+        protected Func<bool> _condition;
+        protected List<INode> _inputNodes;
+        protected List<INode> _outputNodes;
         protected string _name;
         
         public Action Action => _action;
-        public List<ITransition> InputTransitions => _inputTransitions;
-        public List<ITransition> OutputTransitions => _outputTransitions;
+        public Func<bool> Condition => _condition;
+        public List<INode> InputNodes => _inputNodes;
+        public List<INode> OutputNodes =>  _outputNodes;
         public string Name => _name;
         public NodeFSM(string name, Action action = null)
         {
+            _condition = null;
             _action = action;
             _name = name;
-            _inputTransitions = new List<ITransition>();
-            _outputTransitions = new List<ITransition>();
+            _inputNodes = new List<INode>();
+            _outputNodes = new List<INode>();
         }
         public override string ToString()
         {
-            return $"N: {_name} IT: [{TransitionsListToString(_inputTransitions)}]; OT: [{TransitionsListToString(_outputTransitions)}]";
+            return $"N: {_name} IT: [{NodesListToString(_inputNodes)}]; OT: [{NodesListToString(_outputNodes)}]";
         }
 
-        private string TransitionsListToString(List<ITransition> transitions)
+        private string NodesListToString(List<INode> nodes)
         {
             StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < transitions.Count - 1; ++i)
+            for (int i = 0; i < nodes.Count - 1; ++i)
             {
-                builder.Append($"{transitions[i].Name}, ");
+                builder.Append($"{nodes[i].Name}, ");
             }
-            builder.Append($"{transitions[transitions.Count - 1].Name}");
+            builder.Append($"{nodes[nodes.Count - 1].Name}");
             return builder.ToString();
         }
     }
--- Assets/Src/AIBehaviourAPI/FSM/TransitionFSM.cs
+++ Assets/Src/AIBehaviourAPI/FSM/TransitionFSM.cs
@@ -1,25 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace AIBehaviourAPI.FSM
+namespace AIBehaviourAPI.Fsm
 {
     public class TransitionFSM : ITransition
     {
-        private INode _originNode;
-        private INode _destinationNode;
-        private Action _action;
-        private Func<bool> _condition;
-        private string _name;
+        protected List<INode> _originNode;
+        protected List<INode> _destinationNode;
+        protected Action _action;
+        protected Func<bool> _condition;
+        protected string _name;
         
-        public INode OriginNode => _originNode;
-        public INode DestinationNode => _destinationNode;
+        public INode OriginNode => _originNode.Count > 0? _originNode[0] : null;
+        public INode DestinationNode => _destinationNode.Count > 0? _destinationNode[0] : null;
         public Action Action => _action;
         public Func<bool> Condition => _condition;
+        public List<INode> InputNodes => _originNode;
+        public List<INode> OutputNodes => _destinationNode;
         public string Name => _name;
         
         public TransitionFSM(INode originNode, INode destinationNode, string name, Func<bool> condition, Action action = null)
         {
-            _originNode = originNode;
-            _destinationNode = destinationNode;
+            _originNode = new List<INode>();
+            _destinationNode =  new List<INode>();
+            _originNode.Add(originNode);
+            _destinationNode.Add(destinationNode);
+            
             _name = name;
             _condition = condition;
             _action = action;
@@ -27,7 +33,7 @@ namespace AIBehaviourAPI.FSM
 
         public override string ToString()
         {
-            return $"O: {_originNode.Name} -> N: {_name} -> D: {_destinationNode.Name}";
+            return $"O: {(_originNode.Count > 0? _originNode[0].Name : "None")} -> N: {_name} -> D: {(_destinationNode.Count > 0? _destinationNode[0].Name : "None")}";
         }
     }
 }
