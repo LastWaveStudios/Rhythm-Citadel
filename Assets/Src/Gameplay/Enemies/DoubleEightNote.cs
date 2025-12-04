8257d0ec43fdc62021092f6e6ed74de2b94291c6
hotdogwithdog <hotdogwithdog@gmail.com>
2025-12-04 16:30:57 +0100

    Added the normal eight

---
+++ Assets/Src/Gameplay/Enemies/DoubleEightNote.cs
@@ -0,0 +1,30 @@
+﻿namespace Gameplay.Enemies
+{
+    public class DoubleEightNote : AEnemy
+    {
+        protected override void SubscribeToRhythm()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InitializeBehaviour()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void PushDeath()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnRhythmUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Death()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
