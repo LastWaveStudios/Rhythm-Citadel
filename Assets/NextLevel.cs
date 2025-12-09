03392053a9e66954973d72659fc43064ecb1d78d
Ángela Fernández <angela.fyh.16@gmail.com>
2025-12-09 12:45:57 +0100

    v2.4.3 Button Next Level

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,40 @@
+using UI.Menus;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextLevel : MonoBehaviour, IPointerClickHandler
+{
+
+    void Start()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentScene + 1 >= SceneManager.sceneCountInBuildSettings)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        loadNextLevel();
+        
+    }
+
+    public void loadNextLevel()
+    {
+        int currentScene= SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(currentScene);
+        SceneManager.LoadScene(currentScene + 1);
+        /*
+        if (currentScene + 1 < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(currentScene+1);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }*/
+    }
+}
