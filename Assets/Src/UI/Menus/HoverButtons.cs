189b281bf4259b6b971eef590788e0663d543835
picugeo27 <61882288+picugeo27@users.noreply.github.com>
2025-12-09 02:33:51 +0100

    Hovers
    Added hovers to multiple buttons (options, credits, tutorial, level selector)

--- Assets/Src/UI/Menus/HoverButtons.cs
+++ Assets/Src/UI/Menus/HoverButtons.cs
@@ -1,37 +1,27 @@
-using UI.Menus;
-using UI.Menus.States;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class LevelButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+public class HoverButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Image activeImage;
     [SerializeField] private Sprite _normalSprite;
     [SerializeField] private Sprite _hoverSprite;
-
-    [SerializeField] string _myScene;
     void Start()
     {
         activeImage = GetComponent<Image>();
         activeImage.sprite = _normalSprite;
     }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        activeImage.sprite = _normalSprite;
-        MenuManager.Instance.ChangeSceneAndState(_myScene, new UI.Menus.States.Gameplay());
-    }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
-        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         activeImage.sprite = _hoverSprite;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
         activeImage.sprite = _normalSprite;
     }
+
+
+
 }
